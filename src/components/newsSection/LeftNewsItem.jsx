40d74e13f1677d7f3a4bc23d187fd84eb5f8a8a8
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2024-02-12 12:35:19 +0600

    design issue

--- src/components/newsSection/LeftNewsItem.jsx
+++ src/components/newsSection/LeftNewsItem.jsx
@@ -13,7 +13,7 @@ const LeftNewsItem = ({
     <div className={`col-span-12 md:col-span-4 ${className}`}>
       {urlImg && <Img className="w-full" src={urlImg} alt={title} />}
       <a href={url} target="_blank">
-        <h3 className="mb-2.5 text-2xl font-bold lg:text-[28px]">{title}</h3>
+        <h3 className="my-2.5 text-2xl font-bold lg:text-[28px]">{title}</h3>
       </a>
       <p className="text-base text-[#5C5955]">{description}</p>
       <p className="mt-5 text-base text-[#5C5955]">

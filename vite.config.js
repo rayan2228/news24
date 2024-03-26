e6dbd946f1548d3b4aa60192980b68ac491471d0
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2024-03-26 18:00:52 +0600

    origin set

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/v2": "https://newsapi-ruddy-phi.vercel.app"
-    }
-  },
   plugins: [react()],
 })
